acbb7f5ef04b698e354b563a1a4aa7570a771ded
Rbt1c <xyphonesx10x@gmail.com>
2022-07-27 10:30:20 -0400

    yep

--- RedCube (Sage to Monday)/redcube.py
+++ RedCube (Sage to Monday)/redcube.py
@@ -6,29 +6,45 @@ sh = book.sheet_by_index(0)
 sourceFile = open('info.txt','w')
 
 class listInit:
-    def __init__(self, name, number, colum):
+    def __init__(self, name, numberOfRowsRead, colum):
         self.name = name
-        self.number = number
+        self.numberOfRowsRead = numberOfRowsRead
         self.colum = colum
-        for x in range(number):
+
+        numberOfRowsRead  = int(numberOfRowsRead)
+        colum   = int(colum)
+
+        for x in range(numberOfRowsRead):
             cell =  sh.cell_value(rowx = x, colx = colum)
             if cell == "":
                 pass
-            if cell == name:
+            elif cell == name:
                 pass
             else:
                 print("{0} : {1}".format(name, cell), file = sourceFile )
 
+def EndProgram():
+    sourceFile.close()
+    exit()
 
-c1 = listInit("People", 5, 0)
-c2 = listInit("Salary", 5, 1)
-c3 = listInit("Residence", 5, 2)
+def InitLists():
+    nameOfListInit  = input("What is the name of the data you want to process?              :")
+    numberOfRows    = input("How many rows do you wish to print?                            :")
+    columNumber     = input("in what column is the data you want to process? (In decimal)   :")
 
-print("The Fox Jumped Over The Hair!")
+    myList = listInit(nameOfListInit, numberOfRows, columNumber)
 
+    createAnotherList()
 
+def createAnotherList():
+    AnotherList     = input("do you wish to create another list? [Yes/No] (default: No)     :")
+    AnotherList     = str(AnotherList)
+    if AnotherList == "no":
+        EndProgram()
+    elif AnotherList == "yes":
+        InitLists()
 
-sourceFile.close()
+InitLists()
 
 
 

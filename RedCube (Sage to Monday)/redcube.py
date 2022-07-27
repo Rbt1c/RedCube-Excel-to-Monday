d4588dcaf1c050c46c44cbbe93bf1c4776aeb6f7
Rbt1c <xyphonesx10x@gmail.com>
2022-07-27 09:18:12 -0400

    redcube.py

--- RedCube (Sage to Monday)/redcube.py
+++ RedCube (Sage to Monday)/redcube.py
@@ -20,11 +20,7 @@ class listInit:
                 print("{0} : {1}".format(name, cell), file = sourceFile )
 
 
-c1 = listInit("Order Number :", 100, 3)
-c2 = listInit("Order Date   :", 100, 1)
-c3 = listInit("Customer     :", 100, 0)
-c4 = listInit("Item Code    :", 100, 4)
-c5 = listInit("Quantity     :", 100, 9)
+
 
 
 
